515d1eae58c8810198f4d58a3bc6f81d8748d8d9
lindsay culver & alyssa horrocks <student@epicodus.com>
2015-09-10 15:50:54 -0700

    Sort words in order of their appearance and how often they appear.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 var wordOrder = function(phrase) {
   var phraseArray = phrase.toLowerCase().split(" ");
   var wordHash = {}
-  var wordCount = 0;
-
-  for(var i = 0; i < phraseArray.length; i+=1) {
-    var word = phraseArray[i];
-    wordHash[word] = 1;
-  };
 
   phraseArray.forEach(function(word) {
+    wordHash[word] = 1;
+
     var index = phraseArray.indexOf(word);
     for(var j = index + 1; j < phraseArray.length; j += 1) {
+      if ((j === phraseArray.length) && (phraseArray[j] === 1)) {
+        wordHash[word] = 1;
+      };
+
       if (phraseArray[j] === word) {
-        wordHash[word] = 2;
+        wordHash[word] += 1;
       };
     };
   });
--- spec/specs.js
+++ spec/specs.js
@@ -10,4 +10,8 @@ describe('wordOrder', function() {
   it('return the count of a word that appears twice in a phrase', function() {
     expect(wordOrder("this phoenix is a phoenix")).to.eql({'this': 1, 'is': 1, 'a': 1, 'phoenix': 2});
   });
+
+  it('return the count of words that appear twice', function() {
+    expect(wordOrder("the phoenix is the best phoenix")).to.eql({'the': 2, 'is': 1, 'best': 1, 'phoenix': 2});
+  })
 });
