f70862c394e29011131d7db688bd179549aaf838
lindsay culver & alyssa horrocks <student@epicodus.com>
2015-09-10 16:42:39 -0700

    Almost complete. Need to unpack hash.

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,8 @@ var wordOrder = function(phrase) {
 
   phraseArray.forEach(function(word) {
     wordHash[word] = 1;
-
     var index = phraseArray.indexOf(word);
+
     for(var j = index + 1; j < phraseArray.length; j += 1) {
       if ((j === phraseArray.length) && (phraseArray[j] === 1)) {
         wordHash[word] = 1;
@@ -19,3 +19,14 @@ var wordOrder = function(phrase) {
 
   return wordHash;
 };
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    var phrase = $("#input").val();
+    var result = wordOrder(phrase);
+
+    $("#result").text(result);
+    $("#result").show();
+    event.preventDefault();
+  });
+});
